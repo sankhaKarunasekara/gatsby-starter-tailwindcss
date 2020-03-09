5cba64b83f084153ce440c9b0f48fe4bb096db7d
Luke Bennett <hello@lukebennett.com.au>
2020-03-09 22:33:04 +1100

    Style home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,28 +7,24 @@ import SEO from '../components/seo';
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <div className="px-4 mx-auto max-w-7xl sm:px-6 md:px-8">
-      {/* <!-- Replace with your content --> */}
-      <h1 className="text-2xl font-semibold text-gray-900">Dashboard</h1>
-      <div className="py-4">
-        <div className="border-4 border-gray-200 border-dashed rounded-lg">
-          <div className="px-4 py-5 mx-6 my-6 bg-white shadow sm:rounded-lg sm:p-6">
-            <h3 className="text-lg font-medium leading-6 text-gray-900">
-              Contact us
-            </h3>
-            <Link
-              to="/contact"
-              className="mt-1 text-sm font-medium leading-5 text-indigo-600 transition duration-150 ease-in-out hover:text-indigo-500 focus:outline-none focus:underline"
-            >
-              Click here to go to our contact form.
-            </Link>
-            {Array.from(Array(40).keys(), i => (
-              <br key={i} />
-            ))}
-          </div>
+    <h1 className="text-2xl font-semibold text-gray-900">Dashboard</h1>
+    <div className="py-4">
+      <div className="border-4 border-gray-200 border-dashed rounded-lg">
+        <div className="px-4 py-5 mx-6 my-6 bg-white shadow sm:rounded-lg sm:p-6">
+          <h3 className="text-lg font-medium leading-6 text-gray-900">
+            Contact us
+          </h3>
+          <Link
+            to="/contact"
+            className="mt-1 text-sm font-medium leading-5 text-indigo-600 transition duration-150 ease-in-out hover:text-indigo-500 focus:outline-none focus:underline"
+          >
+            Click here to go to our contact form.
+          </Link>
+          {Array.from(Array(40).keys(), i => (
+            <br key={i} />
+          ))}
         </div>
       </div>
-      {/* <!-- /End replace --> */}
     </div>
   </Layout>
 );
